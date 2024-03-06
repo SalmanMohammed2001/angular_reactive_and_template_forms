15b778f643da2ef7b00b99eae01fead5c67c409d
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-03-06 23:09:44 +0530

    sub reactive from done

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -19,6 +19,7 @@ export class ReactiveFormComponent {
 
   form:any;
   emailPattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+  contactRegex='^07\\d{8}$'
 
   constructor() {
     this.form= new FormGroup({
@@ -31,8 +32,19 @@ export class ReactiveFormComponent {
          Validators.pattern(this.emailPattern)
         // Validators.email
       ]),
-      address:new FormControl('',
-        Validators.required),
+
+      address:new FormControl('', Validators.required),
+
+
+      // sub form group
+      contactDetails:new FormGroup({
+        shippingAddress:new FormControl('',[Validators.required]),
+        contactNumber:new FormControl('',[Validators.required,
+        Validators.pattern(this.contactRegex)
+        ] )
+
+      })
+
     })
   }
 
@@ -43,7 +55,14 @@ export class ReactiveFormComponent {
   return this.form.get('email');
   }
   get address(){
-    return this.form.get('email');
+    return this.form.get('address');
+  }
+
+  get shippingAddress(){
+    return this.form.get('contactDetails.shippingAddress');
+  }
+  get contactNumber(){
+    return this.form.get('contactDetails.contactNumber');
   }
 
 
